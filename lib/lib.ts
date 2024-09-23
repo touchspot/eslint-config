668978e9fb8ae05e9c1f79b8b6855a40da7c0a37
('3') <mozisan@users.noreply.github.com>
2024-09-23 09:04:06 +0900

    fix: export `FlatConfig` type

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,3 +1,7 @@
+import type { TSESLint } from "@typescript-eslint/utils";
+
 export { default } from "#pkg/config.js"; // eslint-disable-line import-x/no-default-export
 export * as env from "#pkg/env.js";
 export * as frameworks from "#pkg/frameworks.js";
+
+export type FlatConfig = TSESLint.FlatConfig.ConfigFile;
