8a90741a2f9a101635213a7d4deb043d6277eb29
('3') <mozisan@users.noreply.github.com>
2025-04-15 06:17:42 +0900

    fix: export `Config` type

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,2 +1,3 @@
 export { config as default } from "#pkg/config.js"; // eslint-disable-line import-x/no-default-export
 export * as env from "#pkg/env.js";
+export type { Config } from "typescript-eslint";
