f53711b04a7c2d17ce639129bedb07d50e8efca1
('3') <mozisan@users.noreply.github.com>
2025-04-15 05:43:00 +0900

    feat: upgrade plugins

--- typings/eslint-plugin-check-file/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "eslint-plugin-check-file" {
-	import type { TSESLint } from "@typescript-eslint/utils";
-
-	declare const plugin: TSESLint.FlatConfig.Plugin;
-
-	export default plugin;
-}
