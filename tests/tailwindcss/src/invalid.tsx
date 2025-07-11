fc34d6ba00c40fe1c6e10552ad430536696b1eb5
('3') <mozisan@users.noreply.github.com>
2025-07-11 20:30:39 +0900

    feat: enable `better-tailwindcss/enforce-shorthand-classes`

--- tests/tailwindcss/src/invalid.tsx
+++ tests/tailwindcss/src/invalid.tsx
@@ -4,10 +4,13 @@ export const Invalid: FC = () => (
 	<>
 		<div
 			// prettier-ignore
-			className="w-full h-full" // eslint-disable-line better-tailwindcss/enforce-consistent-class-order
+			className="w-full flex" // eslint-disable-line better-tailwindcss/enforce-consistent-class-order
 		/>
 		<div
 			className="font-[var(--display)]" // eslint-disable-line better-tailwindcss/enforce-consistent-variable-syntax
+		/>
+		<div
+			className="pt-4 pr-4 pb-4 pl-4" // eslint-disable-line better-tailwindcss/enforce-shorthand-classes
 		/>
 		<div
 			className="flex grid" // eslint-disable-line better-tailwindcss/no-conflicting-classes
