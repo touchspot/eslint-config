3442fd0fd5c247cc28b2ba6a055ac30de672485b
('3') <mozisan@users.noreply.github.com>
2025-10-22 12:58:29 +0900

    fix: widen `Config` type to include `EslintConfig.Config[]`

--- lib/lib.ts
+++ lib/lib.ts
@@ -1,6 +1,6 @@
 import type * as EslintConfig from "eslint/config";
 
-export type Config = EslintConfig.Config | readonly EslintConfig.Config[];
+export type Config = EslintConfig.Config | EslintConfig.Config[] | readonly EslintConfig.Config[]; // eslint-disable-line functional/prefer-readonly-type
 
 export { config as default } from "#pkg/config.js"; // eslint-disable-line import-x/no-default-export
 export * as env from "#pkg/env.js";
